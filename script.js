393ff194c9eaa4c2d7c6b8c0dcdfa873abe368e8
Katz <collinkatz@me.com>
2024-12-11 16:44:07 -0600

    pushing a broken site to prod :)

--- script.js
+++ script.js
@@ -2,6 +2,24 @@ const gradientBackground = document.getElementById("gradient-background");
 const pageContent = document.getElementById("page-content");
 const contentContainer = document.getElementById("content-container");
 
+const projects = [
+    {
+        image: "/assets/contact/linkedin.png",
+        title: "Project One",
+        description: "This is a brief description of Project One.",
+    },
+    {
+        image: "/assets/contact/email.png",
+        title: "Project Two",
+        description: "This is a brief description of Project Two.",
+    },
+    {
+        image: "/assets/contact/email.png",
+        title: "Project Two",
+        description: "This is a brief description of Project Two.",
+    }
+];
+
 document.addEventListener("mousemove", (e) => {
     const { clientX: x, clientY: y } = e;
   
@@ -19,16 +37,35 @@ document.querySelectorAll("nav a").forEach(link => {
     link.addEventListener("click", (e) => {
         e.preventDefault();
         const page = e.target.getAttribute("data-page");
-        fetch(`pages/${page}.html`)
-        .then((res) => res.text())
-        .then((text) => {
-            pageContent.innerHTML = text;
-        })
-        .catch((e) => console.error(e));
+        if(page == "portfolio") {
+            pageContent.innerHTML = `<h1 class="text">Portfolio</h1>` + projects
+            .map(
+                (project) => `
+                <div class="project-tab">
+                    <img src="${project.image}" alt="${project.title}" class="project-image">
+                    <div class="project-details">
+                        <h2 class="project-title">${project.title}</h2>
+                        <p class="project-description">${project.description}</p>
+                    </div>
+                    <div class="project-learn-more">
+                        <a href="#" class="learn-more-link">Learn More ➡</a>
+                    </div>
+                </div>
+            `
+            )
+            .join("");
+        } else {
+            fetch(`pages/${page}.html`)
+            .then((res) => res.text())
+            .then((text) => {
+                pageContent.innerHTML = text;
+            })
+            .catch((e) => console.error(e));
+        }
     });
 });
   
-// Initialize with the About Me page content
+// Initialize with the Home page content
 fetch(`pages/home.html`)
     .then((res) => res.text())
     .then((text) => {
